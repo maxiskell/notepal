f2c0e55b6336d477b8cb87505544a61e149bb992
Maxi Skell <maxiskell@gmail.com>
2020-09-02 11:17:00 +0100

    Add Header tests

--- src/components/Notes/Editor.test.tsx
+++ src/components/Notes/Editor.test.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 
 import Editor from "./Editor";
-import { closeEditor } from "../../redux/actions";
 
 test("renders properly", () => {
-  const { container } = render(<Editor close={closeEditor} />);
+  const mockClose = jest.fn();
+  const { container } = render(<Editor close={mockClose} />);
+
   expect(container).toMatchSnapshot();
 });
 
 test("cancel button calls close function", async () => {
   const mockClose = jest.fn();
   const { getByText } = render(<Editor close={mockClose} />);
+
   fireEvent.click(getByText(/Cancel/i));
+
   expect(mockClose).toHaveBeenCalledTimes(1);
 });
---
+++ src/components/Notes/Header.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Header from "./Header";
+
+const mockNewNote = jest.fn();
+
+test("renders properly", () => {
+  const { container } = render(
+    <Header isEditorOpen={false} newNote={mockNewNote} />
+  );
+
+  expect(container).toMatchSnapshot();
+});
+
+test("renders app title", () => {
+  const { getByText } = render(
+    <Header isEditorOpen={false} newNote={mockNewNote} />
+  );
+  const title = getByText(/Notes/i);
+
+  expect(title).toBeInTheDocument();
+});
+
+test("render new editor button if editor is closed", () => {
+  const { getByText } = render(
+    <Header isEditorOpen={false} newNote={mockNewNote} />
+  );
+  const button = getByText(/New Note/i);
+
+  expect(button).toBeInTheDocument();
+});
+
+test("not render new editor button if editor is open", () => {
+  const { queryAllByText } = render(
+    <Header isEditorOpen={true} newNote={mockNewNote} />
+  );
+  const button = queryAllByText(/New Note/i);
+
+  expect(button).toHaveLength(0);
+});
+
+test("new note button calls corresponding function when clicked", () => {
+  const { getByText } = render(
+    <Header isEditorOpen={false} newNote={mockNewNote} />
+  );
+  fireEvent.click(getByText(/New Note/i));
+
+  expect(mockNewNote).toHaveBeenCalledTimes(1);
+});
