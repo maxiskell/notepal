e41992c83680b5cfc49b755a6339c04cd03792ba
Maxi Skell <maxiskell@gmail.com>
2020-09-02 14:17:35 +0100

    Remove mock notes from reducer

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -3,29 +3,8 @@ import { Reducer } from "redux";
 import { INoteState } from "./types";
 import { NoteAction, NoteActionTypes } from "./actions";
 
-const noteList = [
-  {
-    id: 1,
-    title: "The first one",
-    content:
-      "Vivamus quis scelerisque lectus. Aliquam erat volutpat. Quisque vestibulum leo a velit blandit, ac porttitor elit faucibus. Vivamus eu sem pharetra, euismod mauris lobortis, finibus nisl.",
-  },
-  {
-    id: 2,
-    title: "The second one",
-    content:
-      "Sed porta sem ut augue rhoncus auctor. Duis erat ante, venenatis at lectus in, scelerisque tempor dolor. Vivamus massa mauris, bibendum et neque ut, tincidunt luctus risus. Nam lobortis ante sed purus faucibus lobortis. Phasellus faucibus ex orci, nec tincidunt sapien hendrerit eget. ",
-  },
-  {
-    id: 3,
-    title: "The second one",
-    content: "shortie",
-  },
-];
-
 const initialState: INoteState = {
-  // notes: [],
-  notes: noteList,
+  notes: [],
   editorOpen: false,
   currentNoteId: null,
 };
